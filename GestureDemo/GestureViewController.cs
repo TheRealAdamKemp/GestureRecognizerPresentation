e2af3ff1b0f90b46dcabd95ba42ccd1cc6d34542
Adam Kemp <adam@adamkemp.com>
2015-10-19 20:11:49 -0500

    Initial commit.

---
+++ GestureDemo/GestureViewController.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CoreGraphics;
+using UIKit;
+
+namespace GestureDemo
+{
+    public class GestureViewController : UIViewController
+    {
+        #region Constants
+
+        private static readonly CGSize CanvasSize = new CGSize(1000, 1000);
+        private static readonly CGSize ElementSize = new CGSize(50, 50);
+        private static readonly UIColor ScrollViewBackgroundColor = UIColor.LightGray;
+        private static readonly UIColor CanvasBackgroundColor = UIColor.White;
+        private static readonly UIColor ElementColor = UIColor.Blue;
+        private static readonly UIColor SelectedBorderColor = UIColor.Red;
+        private const int SelectedBorderWidth = 2;
+
+        #endregion
+
+        #region View fields
+
+        private UIScrollView _scrollView;
+        private UIView _canvasView;
+
+        #endregion
+
+        #region Gesture state fields
+
+        private readonly HashSet<UIView> _selectedElements = new HashSet<UIView>();
+//        private UIView _touchedSelectedElement;
+
+        #endregion
+
+        #region Constructor
+
+        public GestureViewController()
+        {
+        }
+
+        #endregion
+
+        #region View controller overrides
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            _canvasView = new UIView
+            {
+                BackgroundColor = CanvasBackgroundColor,
+                Bounds = new CGRect(CGPoint.Empty, CanvasSize),
+            };
+
+            _scrollView = new UIScrollView()
+            {
+                ContentSize = CanvasSize,
+                ViewForZoomingInScrollView = sv => _canvasView,
+                BackgroundColor = ScrollViewBackgroundColor,
+            };
+
+            _scrollView.AddSubview(_canvasView);
+
+            View.AddSubview(_scrollView);
+
+            AddGestures();
+
+            AddInitialElements();
+        }
+
+        public override void ViewDidLayoutSubviews()
+        {
+            base.ViewDidLayoutSubviews();
+
+            _scrollView.Frame = View.Bounds;
+            _canvasView.Center = new CGPoint(CanvasSize.Width / 2, CanvasSize.Height / 2);
+        }
+
+        #endregion
+
+        #region Gestures
+
+        private void AddGestures()
+        {
+            // Tap to select
+            var tapGesture = new UITapGestureRecognizer(HandleTap);
+            _canvasView.AddGestureRecognizer(tapGesture);
+
+            // Record whether you touched a selected element
+//            var touchDownGesture = new UILongPressGestureRecognizer(HandleTouchDown) { MinimumPressDuration = 0 };
+//            _canvasView.AddGestureRecognizer(touchDownGesture);
+
+            // Move an element
+            var elementDragGesture = new UIPanGestureRecognizer(HandlePan);
+            _canvasView.AddGestureRecognizer(elementDragGesture);
+
+            elementDragGesture.ShouldReceiveTouch = (g, touch) =>
+            {
+                var locationInCanvas = touch.LocationInView(_canvasView);
+                var touchedElement = ElementUnderPoint(locationInCanvas);
+
+                return touchedElement != null && IsElementSelected(touchedElement);
+            };
+
+//            touchDownGesture.ShouldRecognizeSimultaneously = (g1, g2) => true;
+
+            // Ignore the drag gesture if you didn't touch a selected element
+//            elementDragGesture.ShouldBegin = g => _touchedSelectedElement != null;
+        }
+
+        private void HandleTap(UITapGestureRecognizer gesture)
+        {
+            var locationInCanvas = gesture.LocationInView(_canvasView);
+            var touchedElement = ElementUnderPoint(locationInCanvas);
+            bool didTouchElement = touchedElement != null;
+
+            ClearSelection();
+
+            if (didTouchElement)
+            {
+                SetElementSelected(touchedElement, selected: true);
+            }
+        }
+
+//        private void HandleTouchDown(UILongPressGestureRecognizer gesture)
+//        {
+//            switch (gesture.State)
+//            {
+//                case UIGestureRecognizerState.Began:
+//                    HandleTouchDownBegan(gesture);
+//                    break;
+//                case UIGestureRecognizerState.Changed:
+//                    break;
+//                case UIGestureRecognizerState.Ended:
+//                    CleanupAfterTouchDown();
+//                    break;
+//                case UIGestureRecognizerState.Cancelled:
+//                    CleanupAfterTouchDown();
+//                    break;
+//            }
+//        }
+//
+//        private void HandleTouchDownBegan(UILongPressGestureRecognizer gesture)
+//        {
+//            var locationInCanvas = gesture.LocationInView(_canvasView);
+//            var touchedElement = ElementUnderPoint(locationInCanvas);
+//
+//            if (touchedElement != null && IsElementSelected(touchedElement))
+//            {
+//                _touchedSelectedElement = touchedElement;
+//            }
+//        }
+//
+//        private void CleanupAfterTouchDown()
+//        {
+//            _touchedSelectedElement = null;
+//        }
+
+        private void HandlePan(UIPanGestureRecognizer gesture)
+        {
+            switch (gesture.State)
+            {
+                case UIGestureRecognizerState.Began:
+                    break;
+                case UIGestureRecognizerState.Changed:
+                    HandlePanChanged(gesture);
+                    break;
+                case UIGestureRecognizerState.Ended:
+                    HandlePanEnded(gesture);
+                    CleanupAfterPan();
+                    break;
+                case UIGestureRecognizerState.Cancelled:
+                    CleanupAfterPan();
+                    break;
+            }
+        }
+
+        private void HandlePanChanged(UIPanGestureRecognizer gesture)
+        {
+            var translation = gesture.TranslationInView(_canvasView);
+            var transform = CGAffineTransform.MakeTranslation(translation.X, translation.Y);
+
+            foreach (var view in _selectedElements)
+            {
+                view.Transform = transform;
+            }
+        }
+
+        private void HandlePanEnded(UIPanGestureRecognizer gesture)
+        {
+            var translation = gesture.TranslationInView(_canvasView);
+
+            foreach (var view in _selectedElements)
+            {
+                var center = view.Center;
+                center.X += translation.X;
+                center.Y += translation.Y;
+
+                view.Center = center;
+            }
+        }
+
+        private void CleanupAfterPan()
+        {
+            var identityTransform = CGAffineTransform.MakeIdentity();
+            foreach (var view in _selectedElements)
+            {
+                view.Transform = identityTransform;
+            }
+        }
+
+        #endregion
+
+        #region Element and selection management
+
+        private void AddInitialElements()
+        {
+            const int NumberOfElements = 4;
+            nfloat HorizontalSpacing = ElementSize.Width / 2;
+            nfloat VerticalSpacing = ElementSize.Height / 2;
+
+            var location = new CGPoint(HorizontalSpacing * 2, VerticalSpacing * 2);
+            for (int i = 0; i != NumberOfElements; ++i)
+            {
+                DropNewElement(location);
+
+                location.X += ElementSize.Width + HorizontalSpacing;
+                location.Y += ElementSize.Height + VerticalSpacing;
+            }
+
+            ClearSelection();
+        }
+
+        private void DropNewElement(CGPoint locationInCanvas)
+        {
+            var newElement = new UIView
+            {
+                Bounds = new CGRect(CGPoint.Empty, ElementSize),
+                BackgroundColor = ElementColor,
+            };
+            newElement.Layer.BorderColor = SelectedBorderColor.CGColor;
+
+            newElement.Center = locationInCanvas;
+
+            _canvasView.AddSubview(newElement);
+
+            SetElementSelected(newElement, selected: true);
+        }
+
+        private bool IsElementSelected(UIView view)
+        {
+            return _selectedElements.Contains(view);
+        }
+
+        private bool TryClearSelection()
+        {
+            bool anySelected = _selectedElements.Count != 0;
+
+            if (anySelected)
+            {
+                ClearSelection();
+            }
+
+            return anySelected;
+        }
+
+        private void ClearSelection()
+        {
+            foreach (var view in _selectedElements.ToList())
+            {
+                SetElementSelected(view, selected: false);
+            }
+        }
+
+        private void SetElementSelected(UIView view, bool selected)
+        {
+            if (selected)
+            {
+                _selectedElements.Add(view);
+            }
+            else
+            {
+                _selectedElements.Remove(view);
+            }
+
+            var borderWidth = selected ? SelectedBorderWidth : 0;
+            view.Layer.BorderWidth = borderWidth;
+        }
+
+        private UIView ElementUnderPoint(CGPoint locationInCanvas)
+        {
+            var hitTestView = _canvasView.HitTest(locationInCanvas, null);
+            if (hitTestView == _canvasView)
+            {
+                return null;
+            }
+
+            return hitTestView;
+        }
+
+        #endregion
+
+        #region Utility
+
+        private void CancelScrollGesture()
+        {
+            // Cancel the gesture by disabling and re-enabling the gesture.
+            // This is documented behavior.
+            _scrollView.PanGestureRecognizer.Enabled = false;
+            _scrollView.PanGestureRecognizer.Enabled = true;
+        }
+
+        #endregion
+    }
+}
+
